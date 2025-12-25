c317b8d15d89a39ec0321a3777a8b0a57ad3f8ac
FitConnect User <user@example.com>
2025-12-24 22:45:58 -0500

    Update api.js to use REACT_APP_API_URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Adjustment needed for prod
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', // Adjustment needed for prod
 });
 
 // Add a request interceptor to add the auth token
